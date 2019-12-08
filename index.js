d6805387cda7cc2db81089693dcd535a3d5671ce
mykhailohrynchyshyn <57962221+mykhailohrynchyshyn@users.noreply.github.com>
2019-12-08 17:00:04 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+
+https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+function DNAtoRNA(dna) {
+  return dna.replace (/T/gi, 'U');
+}
+
+https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+function stringToArray(string){
+ return string.split(' ');
+}
+
+https://www.codewars.com/kata/beginner-lost-without-a-map
+
+function maps(x){
+  return x.map(x => x * 2);
+}
+
